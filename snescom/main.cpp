c799256d4ef7e33757fdece868e5df68e9b983a8
Emmanuel Peralta <manz@ringum.net>
2011-06-12 16:31:56 +0200

    Initial Commit

---
+++ snescom/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  snescom
+//
+//  Created by Emmanuel Peralta on 12/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
